8dd33ce167a05944db61c3f15a11a488d80c201e
doffy007 <ochanmimi1@gmail.com>
2021-11-19 22:46:51 +0700

    update terbaru say hello

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package say_hello_go
 
 func SayHello() string {
-	return "Hello Good Luck For You, Keep Up Your Spirit and Make Dream Come True"
+	return "Hello Good Luck For You, Keep Up Your Spirit and Make Dream Come True. ganbate"
 }
