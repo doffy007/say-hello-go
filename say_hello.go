989adf2309ce7ce5ffa015a787b946852d5a74ec
doffy007 <ochanmimi1@gmail.com>
2021-11-19 23:00:53 +0700

    update say hello with parameter name

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package say_hello_go
 
-func SayHello() string {
-	return "Hello Good Luck For You, Keep Up Your Spirit and Make Dream Come True. ganbate"
+func SayHello(name string) string {
+	return "Hello Good Luck " + name + "Thanks for your visit"
 }
